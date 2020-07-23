b5df1123175d8c129ae7672ee46ef2a14057e13b
waltaskew <waltaskew@gmail.com>
2020-07-22 20:02:56 -0700

    implement evaluation for each of our types

---
+++ test/test_wtypes.py
@@ -0,0 +1,55 @@
+"""Tests ensuring types are evaluated properly."""
+
+import pytest  # type: ignore
+
+import wisp.wtypes as wtypes
+
+
+def test_eval_string():
+    """Ensure strings evaluate to themselves."""
+    assert wtypes.String('abc').eval({}) == wtypes.String('abc')
+
+
+def test_eval_integer():
+    """Ensure integers evaluate to themselves."""
+    assert wtypes.Integer(123).eval({}) == wtypes.Integer(123)
+
+
+def test_eval_bool():
+    """Ensure bools evaluate to themselves."""
+    assert wtypes.Bool(True).eval({}) == wtypes.Bool(True)
+
+
+def test_eval_symbol():
+    """Ensure symbols evaluate to their bound value."""
+    env = {'a': wtypes.String('def')}
+    assert wtypes.Symbol('a').eval(env) == wtypes.String('def')
+
+
+def test_eval_missing_symbol():
+    """Ensure an error is raised on missing symbols."""
+    env = {'a': wtypes.String('def')}
+    with pytest.raises(wtypes.WispException):
+        wtypes.Symbol('b').eval(env)
+
+
+def test_eval_list():
+    """Ensure lists are evaluated as post-fix function calls."""
+    env = {'+': wtypes.Function(
+        lambda xs: wtypes.Integer(xs[0].val + xs[1].val)
+    )}
+    res = wtypes.List([
+            wtypes.Integer(1), wtypes.Integer(2), wtypes.Symbol('+')
+    ]).eval(env)
+    assert res == wtypes.Integer(3)
+
+
+def test_eval_empty_list():
+    """Ensure empty lists are evaluated to empty lists."""
+    assert wtypes.List([]).eval({}) == wtypes.List([])
+
+
+def test_eval_list_without_a_func():
+    """Ensure an error is raised when applying non-functions."""
+    with pytest.raises(wtypes.WispException):
+        wtypes.List([wtypes.Integer(1)]).eval({})
--- wisp/wtypes.py
+++ wisp/wtypes.py
@@ -2,9 +2,18 @@ from dataclasses import dataclass
 import typing
 
 
-class Expression:
-    """A wisp expression."""
+class WispException(Exception):
+    """A base exception to distinguish user errors issued by wisp."""
     pass
+
+
+class Expression:
+    """An evaluate-able wisp expression."""
+    def eval(self, env: typing.Dict[str, 'Expression']) -> 'Expression':
+        return self
+
+
+Environment = typing.Dict[str, Expression]
 
 
 @dataclass
@@ -26,12 +35,49 @@ class Bool(Expression):
 
 
 @dataclass
+class Function(Expression):
+    """A wisp function.
+
+    Implemented as a python function which accepts a list of Expressions
+    as arguments and returns an Expression.
+    """
+    func: typing.Callable[[typing.List[Expression]], Expression]
+
+
+@dataclass
 class List(Expression):
     """A wisp list of expressions."""
     lst: typing.List[Expression]
+
+    def eval(self, env: Environment) -> Expression:
+        """Evaluate a list as a postfix function call.
+
+        Treat the last item in the list as a symbol pointing to a function.
+        Treat other members of the list as arguments to the function.
+        An empty list evaluates to an empty list.
+        """
+        if not self.lst:
+            return self
+
+        fn = self.lst[-1].eval(env)
+        if not isinstance(fn, Function):
+            raise WispException('%s is not a function' % self.lst[-1])
+
+        args = [arg.eval(env) for arg in self.lst[:-1]]
+        args.reverse()
+        # mypy gets confused and thinks this is a method call.
+        return fn.func(args)  # type: ignore
 
 
 @dataclass
 class Symbol(Expression):
     """A wisp symbol."""
     name: str
+
+    def eval(self, env: Environment) -> Expression:
+        """Evaluate a symbol by looking up its value in the environment."""
+        val = env.get(self.name)
+        if val is not None:
+            return val
+        else:
+            raise WispException('No binding for %s' % self)
